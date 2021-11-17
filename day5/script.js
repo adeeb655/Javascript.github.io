13c954431762e79fed660c0e6a0eb401fef24431
adeeb655 <mohammad.wajis@successive.tech>
2021-11-17 11:38:23 +0530

    javascript

---
+++ day5/script.js
@@ -0,0 +1,29 @@
+// Task 1 
+let major = {
+    age: function (age) {
+        if (age >= 18) {
+            console.log('He is adult now')
+        }
+        else {
+            console.log('He is under age')
+        }
+    }
+}
+console.log(major.age(17));
+
+// Task 2 
+let shades = {
+    red: "#000000",
+    green: "#f00f00",
+    blue: "#000001",
+}
+// let newarr = []
+// newarr = Object.entries(shades);
+// console.log("users", newarr);
+
+
+let clone = []  //empty array
+for( let arr in shades){ 
+    clone[arr] = shades[arr]
+}
+console.log(clone);
